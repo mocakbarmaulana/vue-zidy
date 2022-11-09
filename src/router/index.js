bb0f5e4501e349bbf212887169036d095368ce3c
akbar <moc.akbar119@gmail.com>
2022-11-09 23:33:14 +0700

    add page chat

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router"
 
+import Chat from "../views/Chat.vue"
 import Home from "../views/Home.vue"
 
 const routes = [
@@ -7,6 +8,11 @@ const routes = [
 		path: "/",
 		name: "Home",
 		component: Home,
+	},
+	{
+		path: "/chat",
+		name: "Chat",
+		component: Chat,
 	},
 ]
 
