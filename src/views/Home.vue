27170e4276a85280766c78da61752eb9c9fde27d
moch akbar maulana <moc.akbar119@gmail.com>
2022-11-11 07:07:31 +0700

    add select and direction chart

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -105,12 +105,51 @@
 		</div>
 	</section>
 
-	<section class="md:p2 px-0">
-		<div class="flex w-full flex-col xl:flex-row min-h-[300px]">
-			<div class="w-full xl:w-[50%] h-full p-10">
+	<section id="chart" class="md:p2 px-0">
+		<div
+			class="px-6 chart__header w-full flex justify-between items-center"
+		>
+			<h3 class="chart__header__text text-[24px] font-medium">
+				Statistic
+			</h3>
+			<div class="flex items-center">
+				<div class="w-[200px] mx-2 md:hidden">
+					<select-fly :options="options" placeholder="Filter" />
+				</div>
+				<div class="w-[200px] mx-2 hidden md:block">
+					<select-fly :options="options" placeholder="Contact" />
+				</div>
+				<div class="w-[200px] mx-2 hidden md:block">
+					<select-fly :options="options" placeholder="Monthly" />
+				</div>
+				<div class="mx-2 hidden md:block w-32">
+					<chart-direction @change-direction="changeDirection" />
+				</div>
+			</div>
+		</div>
+		<div
+			class="chart__content flex w-full min-h-[500px]"
+			:class="{
+				'flex-col': directionChart === 'vertical',
+				'flex-row': directionChart === 'horizontal',
+			}"
+		>
+			<div
+				class="w-full p-10"
+				:class="{
+					'w-1/2': directionChart === 'horizontal',
+					'w-full': directionChart === 'vertical',
+				}"
+			>
 				<chart-component />
 			</div>
-			<div class="w-full xl:w-[50%] h-full p-10">
+			<div
+				class="w-full p-10"
+				:class="{
+					'w-1/2': directionChart === 'horizontal',
+					'w-full': directionChart === 'vertical',
+				}"
+			>
 				<chart-component chart-wrapper="wrapper-two" />
 			</div>
 		</div>
@@ -120,12 +159,54 @@
 <script>
 import { Icon } from "@iconify/vue"
 import ChartComponent from "../components/Chart/ChartComponent.vue"
+import SelectFly from "../components/Select/Select.vue"
+import ChartDirection from "../components/Chart/ChartDirection.vue"
 
 export default {
 	name: "HomeView",
 	components: {
 		Icon,
 		ChartComponent,
+		SelectFly,
+		ChartDirection,
+	},
+	data() {
+		return {
+			options: [
+				{ value: "jan", label: "January" },
+				{ value: "feb", label: "February" },
+				{ value: "mar", label: "March" },
+				{ value: "apr", label: "April" },
+				{ value: "may", label: "May" },
+				{ value: "jun", label: "June" },
+				{ value: "jul", label: "July" },
+				{ value: "aug", label: "August" },
+				{ value: "sep", label: "September" },
+				{ value: "oct", label: "October" },
+				{ value: "nov", label: "November" },
+				{ value: "dec", label: "December" },
+			],
+			directionChart: "horizontal",
+			isMobile: false,
+		}
+	},
+	created() {
+		window.addEventListener("resize", this.handeResize)
+	},
+
+	methods: {
+		changeDirection(value) {
+			this.directionChart = value
+			console.log(value)
+		},
+		handeResize(value) {
+			if (value.target.innerWidth < 768) {
+				this.directionChart = "vertical"
+			} else {
+				this.directionChart = "horizontal"
+			}
+			console.log(value)
+		},
 	},
 }
 </script>
